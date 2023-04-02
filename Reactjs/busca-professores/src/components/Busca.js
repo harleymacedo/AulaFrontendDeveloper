e137e156a6ca9c62ce20d11dcfdef1a3914ea0d4
harleymacedo <harley.ip@gmail.com>
2023-04-02 11:31:38 -0300

    CSS aprimorado

--- Reactjs/busca-professores/src/components/Busca.js
+++ Reactjs/busca-professores/src/components/Busca.js
@@ -11,8 +11,8 @@ const Busca = () => {
     return (
         <div className="divBusca">
             <h3 className="tituloBusca">Busca na estrutura do curso de Sistemas de Informação</h3>
-            <label>Termo: <input type="text" /></label>
-            <button onclick={buscar}>Buscar</button>
+            <label className="label1">Termo: <input type="text" className="input1" /></label>
+            <button onClick={buscar} className="button1">Buscar</button>
         </div>
     )
 }
