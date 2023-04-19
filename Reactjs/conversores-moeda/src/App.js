09b8a07dfad72942e48b24de43aa6009dbd3ee32
harleymacedo <harley.ip@gmail.com>
2023-04-19 13:52:45 -0300

    App React conversor de moeda funcionando

--- Reactjs/conversores-moeda/src/App.js
+++ Reactjs/conversores-moeda/src/App.js
@@ -4,7 +4,9 @@ import Conversor from './components/Conversor';
 const App = () => {
 	return (
 		<div className="App">
-			<Conversor />
+			<Conversor moedaOrigem='USD' moedaDestino='BRL' cor='lightblue' />
+			<Conversor moedaOrigem='CAD' moedaDestino='BRL' cor='yellow' />
+			<Conversor moedaOrigem='EUR' moedaDestino='BRL' cor='lightgreen' />
 		</div>
 	);
 }
--- Reactjs/conversores-moeda/src/components/Conversor.js
+++ Reactjs/conversores-moeda/src/components/Conversor.js
@@ -1,9 +1,28 @@
 import './Conversor.css';
+import {useState} from 'react';
+import axios from 'axios'; 
 
 const Conversor = (props) => {
+
+    const [valorOrigem, setValorOrigem] = useState(0.0);
+    const [valorDestino, setValorDestino] = useState(0.0);
+
+    const atualizarValorOrigem = (event) => {
+        setValorOrigem(event.target.value);
+    }
+
+    const converter = async () => {
+        let url = `https://economia.awesomeapi.com.br/json/last/${props.moedaOrigem}-${props.moedaDestino}`;
+        let result = await axios.get(url);
+        setValorDestino( (parseFloat(valorOrigem) * result.data[`${props.moedaOrigem}${props.moedaDestino}`].bid).toFixed(2) );
+    }
+
     return (
-        <div className="divPrincipal">
-            
+        <div className="divPrincipal" style={{backgroundColor: props.cor}}>
+            <h3>Conversor de {props.moedaOrigem} para {props.moedaDestino}</h3>
+            <label>Moeda Origem: <input value={valorOrigem} onChange={atualizarValorOrigem} /></label>
+            <button onClick={converter}>Converter</button>
+            <h4>Moeda destino: {props.moedaDestino} {valorDestino}</h4>
         </div>
     )
 }
