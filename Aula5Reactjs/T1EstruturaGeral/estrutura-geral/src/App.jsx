b1edd44c9dc5e61d44ab4f7bb88af07c0a683e66
harleymacedo <harley.ip@gmail.com>
2023-08-25 15:44:16 -0300

    Prejeto React com Vite adicionado

---
+++ Aula5Reactjs/T1EstruturaGeral/estrutura-geral/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>        
+      <p className="read-the-docs">
+        Aprenda React com Typescript
+      </p>
+    </>
+  )
+}
+
+export default App
