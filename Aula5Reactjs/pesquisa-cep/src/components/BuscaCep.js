1752bda919139acfecba8fc6fff259ca2714f93f
harleymacedo <harley.ip@gmail.com>
2023-09-20 15:56:15 -0300

    Exemplo de React com busca de CEP

--- Aula5Reactjs/pesquisa-cep/src/components/BuscaCep.js
+++ Aula5Reactjs/pesquisa-cep/src/components/BuscaCep.js
@@ -1,12 +1,29 @@
 import './BuscaCep.css'
+import {useState} from 'react'
+import axios from 'axios'
 
 
 const BuscaCep = (props) => {
+
+    const [valor, setValor] = useState('')
+    const [endereco, setEndereco] = useState('')
+
+    const guardaValor = (event) => {
+        setValor(event.target.value)
+    }
+
+    const buscar = async () => {
+        const url = `https://cep.awesomeapi.com.br/json/${valor}`
+        const result = await axios.get(url)
+        setEndereco(result.data.address)
+    }
+
     return (
         <div style={{backgroundColor: props.cor}}>
             <h2>Cálculo de Frete</h2>
-            <input type="text" placeholder="Digite seu CEP" id="text1" />
-            <input type="button" value="Calcular" />
+            <input type="text" placeholder="Digite seu CEP" id="text1" onKeyUp={guardaValor} />
+            <input type="button" value="Calcular" onClick={buscar} />
+            <p> Endereço: {endereco} </p>
         </div>
     )
 }
