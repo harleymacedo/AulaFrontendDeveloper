df1bd5089167727f34eca2ed4db795ffca324efe
harleymacedo <harley.ip@gmail.com>
2023-10-05 10:52:18 -0300

    Busca por nome e useEffect definido

--- Aula5Reactjs/busca-profs/src/App2.js
+++ Aula5Reactjs/busca-profs/src/App2.js
@@ -1,14 +1,11 @@
 import './App.css'
 import CardProfessor from './CardProfessor'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
 
 function App() {
 
     const [professores, setProfessores] = useState([])
-    const [texto, setTexto] = useState('')
-
-    useEffect( () => {carregar()}, [] )
 
     const carregar = async () => {
         const url = 'https://node-express-api-rest-mock.vercel.app/professor/todos'
@@ -16,22 +13,10 @@ function App() {
         setProfessores(result.data)
     }
 
-    const pesquisar = async () => {
-        const url = 'https://node-express-api-rest-mock.vercel.app/professor/todos'
-        const result = await axios.get(url)
-        const profsFiltrados = result.data.filter( (profAtual) => {return (profAtual.nome === texto)} )
-        setProfessores(profsFiltrados)
-    }
-
-    const atualizarTexto = (event) => {
-        setTexto(event.target.value)
-    }
-
     return (
         <div className="App">
             <header className="App-header">
-                <input type='text' onChange={atualizarTexto} placeholder='Texto de pesquisa' />
-                <input type='button' onClick={pesquisar} value='Pesquisar' />
+                <input type='button' onClick={carregar} value='Carregar' />
                 <h4>Quantidade: { professores.length } </h4>
                 {professores.map( function (professorAtual) {
                     return (
---
+++ Aula5Reactjs/busca-profs/src/ProfessoresModel.js
@@ -0,0 +1,8 @@
+
+var professoresModel = [
+    {"nome": "Joana", "area": "Banco de dados"},
+    {"nome": "Yago", "area": "Estrutura de dados"},
+    {"nome": "Teresa", "area": "Engenharia de software"}
+]
+
+export default professoresModel
