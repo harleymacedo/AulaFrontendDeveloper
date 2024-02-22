ebe6e3527796e4e82d35f1e3bb5f0cd702de83ba
harleymacedo <harley.ip@gmail.com>
2024-02-21 23:55:33 -0300

    Update function done

--- Aula5DeployVersionamento/T1CrudDisciplinas/crud-disciplinas/src/components/Update.jsx
+++ Aula5DeployVersionamento/T1CrudDisciplinas/crud-disciplinas/src/components/Update.jsx
@@ -1,19 +1,47 @@
-
+import { useState } from 'react'
 import './Update.css'
 
 const Update = () => {
 
+    const [codigo, setCodigo] = useState('')
+    const [novoNome, setNovoNome] = useState('')
+    const [novaCH, setNovaCH] = useState('')
+
+    const atualizarCodigo = (event) => {
+        setCodigo(event.target.value)
+    }
+
+    const atualizarNovoNome = (event) => {
+        setNovoNome(event.target.value)
+    }
+
+    const atualizarNovaCH = (event) => {
+        setNovaCH(event.target.value)
+    }
+
     const atualizar = async () => {
-        const url = 'https://aula-backend-developer-fc7o.vercel.app/disciplina'
-        await fetch(url, {method: 'UPDATE'})
+        try {
+            const url = 'https://aula-backend-developer-fc7o.vercel.app/disciplina'
+            await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({codigo: codigo, nome: novoNome, cargaHoraria: novaCH})
+            })    
+        } catch (error) {
+            console.log('Erro detectado')
+        }
+        
     }
 
     return (
         <div className='form1'>
             <h2>Formulário de atualização de disciplinas</h2>
             <form>
-                <input type='text' name='Nome' id='nome' />
-                <input type='text' name='CH' id='ch' />
+                <input type='text' name='codigo' id='codigo' placeholder='Código' onChange={atualizarCodigo} />
+                <input type='text' name='nome' id='nome' placeholder='nome' onChange={atualizarNovoNome} />
+                <input type='text' name='ch' id='ch' placeholder='Carga Horária' onChange={atualizarNovaCH} />
                 <input type='button' value='Atualizar' id='btnAtualizar' onClick={atualizar} />
             </form>
         </div>
