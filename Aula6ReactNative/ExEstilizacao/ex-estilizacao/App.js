562552cf080215eec2e23cdc4686f2fe133b11c1
harleymacedo <harley.ip@gmail.com>
2024-09-05 12:03:49 -0300

    Exemplo de estilização

--- Aula6ReactNative/ExEstilizacao/ex-estilizacao/App.js
+++ Aula6ReactNative/ExEstilizacao/ex-estilizacao/App.js
@@ -1,9 +1,10 @@
-import { StyleSheet, SafeAreaView } from 'react-native'
+import { StyleSheet, SafeAreaView, Text } from 'react-native'
 import Login from './components/Login'
 
 export default function App() {
     return (
         <SafeAreaView style={styles.container}>
+            <Text style={styles.texto1} >Grupos de Estudo</Text>
             <Login />
         </SafeAreaView>
     );
@@ -15,5 +16,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#eee6ff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    texto1: {
+        fontSize: 32,
+        fontFamily: 'Tahoma',
+        color: '#381',
+        marginBottom: 40,
+        fontWeight: '800',
     }
 });
--- Aula6ReactNative/ExEstilizacao/ex-estilizacao/components/Login.js
+++ Aula6ReactNative/ExEstilizacao/ex-estilizacao/components/Login.js
@@ -1,19 +1,54 @@
-import {View, Text, Image, TouchableOpacity, Button, StyleSheet, TextInput} from 'react-native'
+import {View, Text, Alert, TouchableOpacity, Button, StyleSheet, TextInput} from 'react-native'
+import {useState} from 'react'
 
 export default function Login () {
+
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    function atualizarEmail (value) {
+        setEmail(value)
+        console.log(email)
+    }
+
+    function atualizarSenha (value) {
+        setSenha(value)
+    }
+
+    function verificarLogin () {
+        if (email === 'harley.ip@gmail.com' && senha === '123456') {
+            Alert.alert('Login autorizado')
+        } else {
+            Alert.alert('Email ou senha incorrentos')
+        }
+    }
+
     return (
         <View style={styles.containerLogin}>
             <Text style={styles.texto1} >Login no App</Text>
             <Button 
-                title='Cadastrar agora' 
-                style={styles.buttonCadastro} 
+                title='Cadastrar agora'                
             />
             <View>
-                <TextInput placeholder='Email' style={styles.caixaEmail} />
-                <TextInput placeholder='Senha' style={styles.caixaEmail} />
+                <TextInput 
+                    placeholder='Email' 
+                    style={styles.caixaEmailSenha} 
+                    onChangeText={atualizarEmail}
+                />
+                <TextInput 
+                    placeholder='Senha' 
+                    style={styles.caixaEmailSenha} 
+                    secureTextEntry={true}
+                    onChangeText={atualizarSenha}
+                />
             </View>
             <TouchableOpacity style={styles.buttonCadastro}>
-                <Text>Entrar</Text>
+                <Text 
+                    style={{color: 'white', textAlign: 'center', fontSize: 22}}
+                    onPress={verificarLogin}
+                >
+                    Entrar
+                </Text>
             </TouchableOpacity>
         </View>
     )
@@ -34,16 +69,19 @@ const styles = StyleSheet.create({
         fontFamily: 'Verdana',
         fontWeight: '800',
         color: '#939',
-    },
-    buttonCadastro: {
-        backgroundColor: 'green',
-        borderRadius: 4,
-    },
-    caixaEmail: {
+    },    
+    caixaEmailSenha: {
         backgroundColor: 'white',
         borderRadius: 4,
         fontSize: 22,
         width: 200,
         margin: 5,
-    }
+    },
+    buttonCadastro: {
+        backgroundColor: '#4d94ff',
+        borderRadius: 4,
+        height: 30,
+        width: 96,
+        color: 'white',        
+    },
 })
