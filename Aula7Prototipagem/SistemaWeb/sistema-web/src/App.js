b3b43031df921388cca4cd791f7f6d7ff67ca5e2
harleymacedo <harley.ip@gmail.com>
2024-10-30 23:40:30 -0300

    Tela inicial do sistema web em protótipo iniciado

--- Aula7Prototipagem/SistemaWeb/sistema-web/src/App.js
+++ Aula7Prototipagem/SistemaWeb/sistema-web/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Grupos de Estudo do IFCE Campus Crato</h1>
+            <ul>
+                <li>Incentivo</li>
+                <li>Orientação</li>
+                <li>Troca de materiais</li>
+                <li>Troca de dicas</li>
+                <li>Resolução de problemas</li>
+            </ul>            
+        </div>
+    )
 }
 
-export default App;
+export default App
