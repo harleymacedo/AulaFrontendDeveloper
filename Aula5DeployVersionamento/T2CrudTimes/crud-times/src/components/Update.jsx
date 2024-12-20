2454abc1b5ce6fa2c1a18f4437386da8b98cf5e7
harleymacedo <harley.ip@gmail.com>
2024-12-19 23:44:19 -0300

    Componente update criado

---
+++ Aula5DeployVersionamento/T2CrudTimes/crud-times/src/components/Update.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function Update () {
+    return (
+        <div className='divPrincipalUpdate'>
+            <h2>Componente Update</h2>
+        </div>
+    )
+}
+
+export default Update
