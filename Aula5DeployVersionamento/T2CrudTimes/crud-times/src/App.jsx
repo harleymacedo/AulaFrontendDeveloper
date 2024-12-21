85c586a85064bc134dffda9bf6d8094cfac873a1
harleymacedo <harley.ip@gmail.com>
2024-12-20 23:45:04 -0300

    Componentes do CRUD adicionados

--- Aula5DeployVersionamento/T2CrudTimes/crud-times/src/App.jsx
+++ Aula5DeployVersionamento/T2CrudTimes/crud-times/src/App.jsx
@@ -3,16 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Read from './components/Read'
 import Create from './components/Create'
+import Read from './components/Read'
+import Update from './components/Update'
+import Delete from './components/Delete'
 
 function App() {
 
     return (
         <>
             <h1>Sistema de gerenciamento de times</h1>
-            <Read />
             <Create />
+            <Read />
+            <Update />
+            <Delete />
         </>
     )
 }
