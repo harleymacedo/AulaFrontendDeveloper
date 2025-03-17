c8511b5461d7d9a41478e7df87f289fa0bee67db
harleymacedo <harley.ip@gmail.com>
2025-03-17 15:37:50 -0300

    Navegação iniciada e pdf da aula atualizado

--- Aula5DeployVersionamento/Projeto1GruposEstudo/gruposestudo/src/main.jsx
+++ Aula5DeployVersionamento/Projeto1GruposEstudo/gruposestudo/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Login from './components/login/Login'
-import Grupo from './components/grupo/Grupo'
-import Aluno from './components/aluno/Aluno'
+import Login from './components/login/Login.jsx'
+import Grupo from './components/grupo/Grupo.jsx'
+import Aluno from './components/aluno/Aluno.jsx'
 import { createBrowserRouter, RouterProvider} from "react-router-dom"
 
 const router = createBrowserRouter([
@@ -28,8 +28,19 @@ const router = createBrowserRouter([
   }, 
 ])
 
+const router2 = (<BrowserRouter>
+<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="dashboard" element={<Dashboard />}>
+    <Route index element={<RecentActivity />} />
+    <Route path="project/:id" element={<Project />} />
+  </Route>
+</Routes>
+</BrowserRouter>)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    {router2}
   </StrictMode>,
 )
