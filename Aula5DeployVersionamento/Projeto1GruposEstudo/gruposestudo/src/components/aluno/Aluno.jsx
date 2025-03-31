1c7509392b0bb08c16ac5907fca34cd780aaafa7
harleymacedo <harley.ip@gmail.com>
2025-03-31 11:57:06 -0300

    Icones adicionados em algumas telas

--- Aula5DeployVersionamento/Projeto1GruposEstudo/gruposestudo/src/components/aluno/Aluno.jsx
+++ Aula5DeployVersionamento/Projeto1GruposEstudo/gruposestudo/src/components/aluno/Aluno.jsx
@@ -1,5 +1,8 @@
 import './Aluno.css'
 import Navbar from '../navbar/Navbar'
+import { FaCirclePlus,  } from 'react-icons/fa6'
+import { CiEdit } from "react-icons/ci"
+import { CiCircleRemove } from "react-icons/ci"
 
 export default function Aluno() {
     return (
@@ -10,15 +13,16 @@ export default function Aluno() {
                     <h2>Aluno</h2>
                 </div>
                 <div className='alunoForm'>
+                    <p> <FaCirclePlus /> </p>
                     <table className='alunoTable'>
                         <tr>
-                            <th>Nome</th><th>Cidade</th><th>Grupos</th>
+                            <th>Nome</th><th>Cidade</th><th>Grupos</th><th>Ações</th>
                         </tr>
                         <tr>
-                            <td>Ana</td><td>Crato</td><td>Python, Javascript, Mobile, Web</td>
+                            <td>Ana</td><td>Crato</td><td>Python, Javascript, Mobile, Web</td><td> <CiEdit /> <CiCircleRemove /> </td>
                         </tr>
                         <tr>
-                            <td>Marcos</td><td>Crato</td><td>Javascript, AWS</td>
+                            <td>Marcos</td><td>Crato</td><td>Javascript, AWS</td><td> <CiEdit /> <CiCircleRemove /> </td>
                         </tr>
                     </table>
                 </div>
--- Aula5DeployVersionamento/Projeto1GruposEstudo/gruposestudo/src/components/evento/Evento.jsx
+++ Aula5DeployVersionamento/Projeto1GruposEstudo/gruposestudo/src/components/evento/Evento.jsx
@@ -1,5 +1,8 @@
 import './Evento.css'
 import Navbar from '../navbar/Navbar'
+import { FaCirclePlus,  } from 'react-icons/fa6'
+import { CiEdit } from "react-icons/ci"
+import { CiCircleRemove } from "react-icons/ci"
 
 export default function Evento() {
     return (
@@ -10,15 +13,16 @@ export default function Evento() {
                     <h2>Evento</h2>
                 </div>
                 <div className='eventoForm'>
+                    <p> <FaCirclePlus /> </p>
                     <table className='eventoTable'>
                         <tr>
-                            <th>Nome</th><th>Local</th><th>Data</th>
+                            <th>Nome</th><th>Local</th><th>Data</th> <tr>Ações</tr>
                         </tr>
                         <tr>
-                            <td>Python</td><td>Crato</td><td>01/01/2022</td>
+                            <td>Python</td><td>Crato</td><td>01/01/2022</td><td> <CiEdit /> <CiCircleRemove /> </td>
                         </tr>
                         <tr>
-                            <td>Javascript</td><td>Crato</td><td>01/01/2022</td>
+                            <td>Javascript</td><td>Crato</td><td>01/01/2022</td><td> <CiEdit /> <CiCircleRemove /> </td>
                         </tr>
                     </table>
                 </div>
