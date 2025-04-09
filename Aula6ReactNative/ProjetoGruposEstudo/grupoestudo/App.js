fdb9e7f95fa242fc35132c26265e0d426b7b5d0b
harleymacedo <harley.ip@gmail.com>
2025-04-08 23:11:55 -0300

    Projeto grupos de estudo criado

--- Aula6ReactNative/ProjetoGruposEstudo/grupoestudo/App.js
+++ Aula6ReactNative/ProjetoGruposEstudo/grupoestudo/App.js
@@ -4,10 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>App Teste aula</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-    
   );
 }
 
