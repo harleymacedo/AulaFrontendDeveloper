50010383b90b38a774fdd9dd3e8bbb9d82413c60
harleymacedo <harley.ip@gmail.com>
2025-04-17 23:53:06 -0300

    Lista de grupos iniciada

--- Aula6ReactNative/ProjetoGruposEstudo/grupoestudo/components/Grupos.js
+++ Aula6ReactNative/ProjetoGruposEstudo/grupoestudo/components/Grupos.js
@@ -5,8 +5,18 @@ import { useState, useEffect } from 'react'
 export default function Grupos() {
 
   useEffect(function () {
-    
+    obterGrupos()
   }, [])
+
+  async function obterGrupos () {
+    try {
+      const response = await fetch('https://gruposdeestudobackend.vercel.app/grupo')
+      const data = await response.json()
+      setGrupos(data)
+    } catch (error) {
+      setMensagemErro('Erro ao obter os grupos')
+    }
+  }
 
   const [grupos, setGrupos] = useState([])
 
@@ -14,6 +24,12 @@ export default function Grupos() {
     <View style={styles.container}>
       <Text style={styles.text}>Grupos</Text>
       <Text style={styles.text}>Lista de grupos</Text>
+      {grupos.map((grupo) => (
+        <View key={grupo._id} style={styles.container}>
+          <Text style={styles.text}>{grupo.nome}</Text>
+          <Text style={styles.text}>{grupo.curso}</Text>
+        </View>
+      ))}
     </View>
   )
 }
