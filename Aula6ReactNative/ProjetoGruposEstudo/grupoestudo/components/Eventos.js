8fb044e720d15101b61a57b1f64395b280eea3ae
harleymacedo <harley.ip@gmail.com>
2025-04-22 21:30:53 -0300

    Tela de eventos melhorada

--- Aula6ReactNative/ProjetoGruposEstudo/grupoestudo/components/Eventos.js
+++ Aula6ReactNative/ProjetoGruposEstudo/grupoestudo/components/Eventos.js
@@ -1,25 +1,52 @@
 import { View, Text, StyleSheet } from 'react-native'
 
 export default function Eventos() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Eventos</Text>
-      <Text style={styles.text}>Lista de eventos</Text>
-    </View>
-  )
+
+    const [eventos, setEventos] = useState([])
+    const [mensagem, setMensagem] = useState('')
+
+    useEffect(function () {
+        obterEventos()
+    }, [])
+
+    async function obterEventos() {
+        try {
+            const response = await fetch('https://gruposdeestudobackend.vercel.app/evento')
+            const data = await response.json()
+            setEventos(data)
+        } catch (error) {
+            setMensagemErro('Erro ao obter os eventos')
+        }
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Eventos</Text>
+            <Text style={styles.text}>Lista de eventos</Text>
+            {mensagem && <Text>{mensagem}</Text>}
+            {eventos.length === 0 && <Text style={styles.text}>Nenhum evento encontrado</Text>}
+            {eventos.map((evento) => (
+                <View key={evento._id} style={styles.container}>
+                    <Text style={styles.text}>{evento.nome}</Text>
+                    <Text style={styles.text}>{evento.data}</Text>
+                </View>
+            ))}
+        </View>
+    )
 }
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    padding: 10,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-  },
-  text: {
-    fontSize: 18,
-    color: '#333',
-  },
+    container: {
+        backgroundColor: '#fff',
+        padding: 10,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+    },
+    text: {
+        fontSize: 18,
+        color: '#333',
+    },
 })
--- Aula6ReactNative/ProjetoGruposEstudo/grupoestudo/components/Grupos.js
+++ Aula6ReactNative/ProjetoGruposEstudo/grupoestudo/components/Grupos.js
@@ -4,50 +4,50 @@ import { useState, useEffect } from 'react'
 
 export default function Grupos() {
 
-  useEffect(function () {
-    obterGrupos()
-  }, [])
+    const [grupos, setGrupos] = useState([])
+    const [mensagem, setMensagem] = useState('')
 
-  async function obterGrupos () {
-    try {
-      const response = await fetch('https://gruposdeestudobackend.vercel.app/grupo')
-      const data = await response.json()
-      setGrupos(data)
-    } catch (error) {
-      setMensagemErro('Erro ao obter os grupos')
+    useEffect(function () {
+        obterGrupos()
+    }, [])
+
+    async function obterGrupos() {
+        try {
+            const response = await fetch('https://gruposdeestudobackend.vercel.app/grupo')
+            const data = await response.json()
+            setGrupos(data)
+        } catch (error) {
+            setMensagemErro('Erro ao obter os grupos')
+        }
     }
-  }
 
-  const [grupos, setGrupos] = useState([])
-  const [mensagem, setMensagem] = useState('')
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Grupos</Text>
-      <Text style={styles.text}>Lista de grupos</Text>
-      {mensagem && <Text>{mensagem}</Text>}
-      {grupos.length === 0 && <Text style={styles.text}>Nenhum grupo encontrado</Text>}
-      {grupos.map((grupo) => (
-        <View key={grupo._id} style={styles.container}>
-          <Text style={styles.text}>{grupo.nome}</Text>
-          <Text style={styles.text}>{grupo.curso}</Text>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Grupos</Text>
+            <Text style={styles.text}>Lista de grupos</Text>
+            {mensagem && <Text>{mensagem}</Text>}
+            {grupos.length === 0 && <Text style={styles.text}>Nenhum grupo encontrado</Text>}
+            {grupos.map((grupo) => (
+                <View key={grupo._id} style={styles.container}>
+                    <Text style={styles.text}>{grupo.nome}</Text>
+                    <Text style={styles.text}>{grupo.curso}</Text>
+                </View>
+            ))}
         </View>
-      ))}
-    </View>
-  )
+    )
 }
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    padding: 10,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-  },
-  text: {
-    fontSize: 18,
-    color: '#333',
-  },
+    container: {
+        backgroundColor: '#fff',
+        padding: 10,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+    },
+    text: {
+        fontSize: 18,
+        color: '#333',
+    },
 })
