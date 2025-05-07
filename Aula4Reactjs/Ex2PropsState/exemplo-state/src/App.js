6fba0c6f47faa5a87337d867e74fd5e0537e883e
harleymacedo <harley.ip@gmail.com>
2025-05-07 16:48:28 -0300

    Melhorias em estado e props de app react.

--- Aula4Reactjs/Ex2PropsState/exemplo-state/src/App.js
+++ Aula4Reactjs/Ex2PropsState/exemplo-state/src/App.js
@@ -1,13 +1,47 @@
 import './App.css'
 import Post from './components/Post'
+import { useState } from 'react'
 
 function App() {
+
+    const [totalPontos, setTotalPontos] = useState(0)
+
+    function somarPontosTotais() {
+        setTotalPontos(totalPontos + 1)
+    }
+    function subtrairPontosTotais() {
+        if (totalPontos > 0) {
+            setTotalPontos(totalPontos - 1)
+        }
+    }
+
     return (
         <div className="App">
-            <Post pensamento='Pensamento A' cor='silver' />
-            <Post pensamento='Pensamento B' cor='yellow' />
-            <Post pensamento='Pensamento C' cor='pink' />
-            <Post pensamento='Pensamento D' cor='gray' />
+            <h1>Total: {totalPontos} </h1>
+            <Post 
+                pensamento='Pensamento A' 
+                cor='silver' 
+                somarTotais={somarPontosTotais} 
+                subtrairTotais={subtrairPontosTotais} 
+            />
+            <Post 
+                pensamento='Pensamento B' 
+                cor='yellow' 
+                somarTotais={somarPontosTotais} 
+                subtrairTotais={subtrairPontosTotais} 
+            />
+            <Post 
+                pensamento='Pensamento C' 
+                cor='pink' 
+                somarTotais={somarPontosTotais} 
+                subtrairTotais={subtrairPontosTotais} 
+            />
+            <Post 
+                pensamento='Pensamento D' 
+                cor='gray' 
+                somarTotais={somarPontosTotais} 
+                subtrairTotais={subtrairPontosTotais} 
+            />
         </div>
     )
 }
--- Aula4Reactjs/Ex2PropsState/exemplo-state/src/components/Post.js
+++ Aula4Reactjs/Ex2PropsState/exemplo-state/src/components/Post.js
@@ -1,20 +1,38 @@
 import { useState } from 'react'
 
-function Post (props) {
+export default function Post (props) {
 
     const [pontos, setPontos] = useState(0)
+
+    function somarPontos() {
+        setPontos(pontos + 1)
+        props.somarTotais()
+    }
+
+    function subtrairPontos() {
+        if (pontos > 0) {
+            setPontos(pontos - 1)
+            props.subtrairTotais()
+        }
+    }
 
     return (
         <div style={{backgroundColor: props.cor}}>
             <h2>Novo Post</h2>
             <p> {props.pensamento} </p>
-            <input type='button' value='+ pontos' onClick={ () => {setPontos(pontos + 1)} } />
-            <input type='button' value='- pontos' onClick={ () => { if (pontos > 0) setPontos(pontos - 1)} } />
+            <input 
+                type='button' 
+                value='+ pontos' 
+                onClick={somarPontos} 
+            />
+            <input 
+                type='button' 
+                value='- pontos' 
+                onClick={subtrairPontos} 
+            />
             <h3> {pontos} </h3>
             <h6> Pontos: {pontos} </h6>
             <h2> Points: {pontos} </h2>            
         </div>
     )
 }
-
-export default Post
